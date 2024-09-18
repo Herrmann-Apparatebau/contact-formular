84fc0a913a31d69bf3dde2b00c2ce5fd6094541b
Krischan-Klug <krischanklug@icloud.com>
2024-09-18 16:29:28 +0200

    add styling to form

--- src/components/create.js
+++ src/components/create.js
@@ -1,3 +1,20 @@
+import styled from "styled-components";
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  margin-top: 20px;
+  max-width: 300px;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+`;
+
+const StyledInput = styled.input`
+  margin-bottom: 10px;
+`;
+
 export default function Create() {
   let finalUser = {};
   const handleSubmit = (e) => {
@@ -30,59 +47,69 @@ export default function Create() {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <StyledForm onSubmit={handleSubmit}>
         <h1>Contact</h1>
         <label htmlFor="firstName">First name</label>
-        <input type="text" name="firstName" id="firstName" required />
+        <StyledInput type="text" name="firstName" id="firstName" required />
+
         <label htmlFor="lastName">Last name</label>
-        <input type="text" name="lastName" id="lastName" required />
+        <StyledInput type="text" name="lastName" id="lastName" required />
+
         <label htmlFor="address">Address</label>
-        <input type="text" name="address" id="address" required />
+        <StyledInput type="text" name="address" id="address" required />
+
         <label htmlFor="zipCode">Zipcode</label>
-        <input type="text" name="zipCode" id="zipCode" required />
+        <StyledInput type="text" name="zipCode" id="zipCode" required />
+
         <label htmlFor="city">City</label>
-        <input type="text" name="city" id="city" required />
+        <StyledInput type="text" name="city" id="city" required />
+
         <label htmlFor="country">Country</label>
-        <input type="text" name="country" id="country" required />
+        <StyledInput type="text" name="country" id="country" required />
+
         <label htmlFor="email">E-Mail</label>
-        <input type="email" name="email" id="email" />
+        <StyledInput type="email" name="email" id="email" />
+
         <label htmlFor="phone">Phone</label>
-        <input type="text" name="phone" id="phone" />
+        <StyledInput type="text" name="phone" id="phone" />
+        <br />
+        <div>
+          <label htmlFor="ozon">Ozon</label>
+          <input type="checkbox" name="ozon" id="ozon" value={"true"} />
+          <label htmlFor="colon">Colon</label>
+          <input type="checkbox" name="colon" id="colon" value={"true"} />
+          <label htmlFor="veterinary">Veterinary</label>
+          <input
+            type="checkbox"
+            name="veterinary"
+            id="veterinary"
+            value={"true"}
+          />
+          <label htmlFor="disposables">Disposables</label>
+          <input
+            type="checkbox"
+            name="disposables"
+            id="disposables"
+            value={"true"}
+          />
+        </div>
         <br />
 
-        <label htmlFor="ozon">Ozon</label>
-        <input type="checkbox" name="ozon" id="ozon" value={"true"} />
-        <label htmlFor="colon">Colon</label>
-        <input type="checkbox" name="colon" id="colon" value={"true"} />
-        <label htmlFor="veterinary">Veterinary</label>
-        <input
-          type="checkbox"
-          name="veterinary"
-          id="veterinary"
-          value={"true"}
-        />
-        <label htmlFor="disposables">Disposables</label>
-        <input
-          type="checkbox"
-          name="disposables"
-          id="disposables"
-          value={"true"}
-        />
-        <br />
-
-        <label htmlFor="dsgvo">DSGVO</label>
-        <input
-          type="checkbox"
-          name="dsgvo"
-          id="dsgvo"
-          required
-          value={"true"}
-        />
+        <div>
+          <label htmlFor="dsgvo">DSGVO</label>
+          <input
+            type="checkbox"
+            name="dsgvo"
+            id="dsgvo"
+            required
+            value={"true"}
+          />
+        </div>
 
         <br />
 
         <button type="submit">Create</button>
-      </form>
+      </StyledForm>
     </>
   );
 }
