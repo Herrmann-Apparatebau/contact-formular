561f6b0927cb13b8de4382514746055d2d97c4f0
Krischan-Klug <krischanklug@icloud.com>
2024-09-19 09:12:42 +0200

    first styling

--- src/components/create.js
+++ src/components/create.js
@@ -1,4 +1,17 @@
 import styled from "styled-components";
+import Image from "next/image";
+
+import logo from "/public/logo.jpg";
+import Link from "next/link";
+
+const StyledFormWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  padding-top: 20px;
+  padding-bottom: 20px;
+`;
 
 const StyledForm = styled.form`
   display: flex;
@@ -9,20 +22,39 @@ const StyledForm = styled.form`
   padding: 20px;
   border: 1px solid #ccc;
   border-radius: 5px;
+  background-color: #f2f2f2;
 `;
 
 const StyledInput = styled.input`
   margin-bottom: 10px;
 `;
 
+const StyledImageWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 10px;
+`;
+
 export default function Create() {
   let finalUser = {};
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
     // get form data
     const formData = new FormData(e.target);
-    const userObject = Object.fromEntries(formData.entries());
+
+    // get checked interests
+    const interests = Array.from(formData.getAll("interests")).filter(
+      (interest) => interest !== ""
+    );
+
+    // combine user object
+    const userObject = {
+      ...Object.fromEntries(formData.entries()),
+      interests,
+    };
 
     finalUser = userObject;
     createUser();
@@ -47,69 +79,88 @@ export default function Create() {
 
   return (
     <>
-      <StyledForm onSubmit={handleSubmit}>
-        <h1>Contact</h1>
-        <label htmlFor="firstName">First name</label>
-        <StyledInput type="text" name="firstName" id="firstName" required />
+      <StyledFormWrapper>
+        <StyledForm onSubmit={handleSubmit}>
+          <StyledImageWrapper>
+            <Image src={logo} alt="logo" width={100} height={100} />
+          </StyledImageWrapper>
+          <label htmlFor="company">Company</label>
+          <StyledInput type="text" name="company" id="company" required />
 
-        <label htmlFor="lastName">Last name</label>
-        <StyledInput type="text" name="lastName" id="lastName" required />
+          <label htmlFor="title">Title</label>
+          <StyledInput type="text" name="title" id="title" required />
 
-        <label htmlFor="address">Address</label>
-        <StyledInput type="text" name="address" id="address" required />
+          <label htmlFor="firstName">First name*</label>
+          <StyledInput type="text" name="firstName" id="firstName" required />
 
-        <label htmlFor="zipCode">Zipcode</label>
-        <StyledInput type="text" name="zipCode" id="zipCode" required />
+          <label htmlFor="lastName">Last name*</label>
+          <StyledInput type="text" name="lastName" id="lastName" required />
 
-        <label htmlFor="city">City</label>
-        <StyledInput type="text" name="city" id="city" required />
+          <label htmlFor="address">Address*</label>
+          <StyledInput type="text" name="address" id="address" required />
 
-        <label htmlFor="country">Country</label>
-        <StyledInput type="text" name="country" id="country" required />
+          <label htmlFor="zipCode">Zipcode*</label>
+          <StyledInput type="text" name="zipCode" id="zipCode" required />
 
-        <label htmlFor="email">E-Mail</label>
-        <StyledInput type="email" name="email" id="email" />
+          <label htmlFor="city">City*</label>
+          <StyledInput type="text" name="city" id="city" required />
 
-        <label htmlFor="phone">Phone</label>
-        <StyledInput type="text" name="phone" id="phone" />
-        <br />
-        <div>
-          <label htmlFor="ozon">Ozon</label>
-          <input type="checkbox" name="ozon" id="ozon" value={"true"} />
-          <label htmlFor="colon">Colon</label>
-          <input type="checkbox" name="colon" id="colon" value={"true"} />
-          <label htmlFor="veterinary">Veterinary</label>
-          <input
-            type="checkbox"
-            name="veterinary"
-            id="veterinary"
-            value={"true"}
-          />
-          <label htmlFor="disposables">Disposables</label>
-          <input
-            type="checkbox"
-            name="disposables"
-            id="disposables"
-            value={"true"}
-          />
-        </div>
-        <br />
+          <label htmlFor="country">Country*</label>
+          <StyledInput type="text" name="country" id="country" required />
 
-        <div>
-          <label htmlFor="dsgvo">DSGVO</label>
-          <input
-            type="checkbox"
-            name="dsgvo"
-            id="dsgvo"
-            required
-            value={"true"}
-          />
-        </div>
+          <label htmlFor="email">E-Mail</label>
+          <StyledInput type="email" name="email" id="email" />
 
-        <br />
+          <label htmlFor="phone">Phone</label>
+          <StyledInput type="text" name="phone" id="phone" />
+          <br />
+          <div>
+            <label htmlFor="ozon">Ozon</label>
+            <input type="checkbox" name="interests" id="ozon" value="ozon" />
 
-        <button type="submit">Create</button>
-      </StyledForm>
+            <label htmlFor="colon">Colon</label>
+            <input type="checkbox" name="interests" id="colon" value="colon" />
+
+            <label htmlFor="veterinary">Veterinary</label>
+            <input
+              type="checkbox"
+              name="interests"
+              id="veterinary"
+              value="veterinary"
+            />
+
+            <label htmlFor="disposables">Disposables</label>
+            <input
+              type="checkbox"
+              name="interests"
+              id="disposables"
+              value="disposables"
+            />
+          </div>
+          <br />
+
+          <div>
+            <Link
+              href="/HAB_Datenschutzerklaerung_2019_12.pdf"
+              target="_blank"
+              locale={false}
+            >
+              Privacy Policy
+            </Link>
+            <input
+              type="checkbox"
+              name="dsgvo"
+              id="dsgvo"
+              required
+              value={"true"}
+            />
+          </div>
+
+          <br />
+
+          <button type="submit">Create</button>
+        </StyledForm>
+      </StyledFormWrapper>
     </>
   );
 }
