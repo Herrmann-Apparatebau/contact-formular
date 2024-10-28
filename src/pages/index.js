a38b96a67d8afb853e275e6ad691674ddf64e230
Krischan-Klug <krischanklug@icloud.com>
2024-10-28 16:00:40 +0100

    add language selection styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 import { useRouter } from "next/router";
+import Image from "next/image";
 
 const StyledImageWrapper = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   justify-content: center;
   align-items: center;
-  min-height: 100vh;
   box-sizing: border-box;
 `;
 
@@ -19,17 +19,34 @@ const StyledImageBox = styled.div`
   padding: 10px;
 `;
 
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  min-height: 50vh;
+`;
+
+const StyledH2 = styled.h2`
+  margin: 10px;
+`;
+
 export default function Home() {
   const router = useRouter();
   return (
     <>
+      <HeaderWrapper>
+        <Image src="/logo.jpg" alt="logo" width={100} height={100}></Image>
+        <StyledH2>Sprachauswahl</StyledH2>
+        <StyledH2>Select your language</StyledH2>
+      </HeaderWrapper>
       <StyledImageWrapper>
         <StyledImageBox>
           <StyledImage
             src="/flags/DE.png"
             alt="DE"
-            width={150}
-            height={150}
+            width={80}
+            height={80}
             onClick={() => router.push("/de")}
           ></StyledImage>
         </StyledImageBox>
@@ -37,8 +54,8 @@ export default function Home() {
           <StyledImage
             src="/flags/EN.png"
             alt="EN"
-            width={150}
-            height={150}
+            width={80}
+            height={80}
             onClick={() => router.push("/en")}
           ></StyledImage>
         </StyledImageBox>
